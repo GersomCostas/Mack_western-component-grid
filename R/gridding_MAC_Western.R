d8109088937d4857521a97d293846db9dc7c2baf
Gersom Costas <gersom.costas@vi.ieo.es>
2017-10-27 12:27:33 +0200

    updating code

--- R/gridding_MAC_Western.R
+++ R/gridding_MAC_Western.R
@@ -1,13 +1,14 @@
-################################################################################
-################################################################################
+############################################
 ## SCRIPT : ANNUAL EGG PRODUCTION FOR NEA MACKEREL. GRIDDING WESTERN AREA 
-## version SEPTEMBER 2017
+## version October 2017
 ##
-## Gersom Costas (Instituto Espanol de Oceanografia)
+## Gersom Costas 
 ## gersom.costas@ieo.es
-################################################################################
-################################################################################
-##DESCRIPTION 
+#
+############################################
+#
+##DESCRIPTION
+#
 # The  mackerel and western horse mackerel egg surveys (WGMEGG) cover spawning
 # areas for NEA mackerel and Western horse mackerel stocks. 
 #The north-east Atlantic shelf area is sub-divided  into two spawning areas: 
@@ -19,7 +20,7 @@
 #from 43 degrees N to 63 degrees N with same western boundary as for mackerel(ICES, 2016)
 #The basic sampling unit is 0.5 degree longitude * 0.5 degree latitude, half of an ICES rectangle.
 
-#######################################################################################################
+############################################
 
 ##  SPATIAL GRID WESTERN AREA  
 
@@ -27,14 +28,20 @@
 
 #Latitude: lower or equal 68.5 degrees N   y bigger or equal 44 degrees N  
 #Longitude: bigger or equal 0 degrees W    y lower or equal 23 degrees W 
- #######################################################################################################
+
+############################################
+
+
 # clear workspace
+
 rm(list = ls())
 
 
 ##################
 # load libraries
 ##################
+
+
 if(!require(plyr)) install.packages("plyr") ; require(plyr)
 if(!require(sp)) install.packages("sp") ; require(sp)
 if(!require(maptools)) install.packages("maptools") ; require(maptools)
@@ -43,65 +50,102 @@ if(!require(maps)) install.packages("maps") ; require(maps)
 if(!require(mapdata)) install.packages("mapdata") ; require(mapdata)
 
 #Importing the mackerel Western area rectangles which  have been sampled along survey temporal series. 
-#Rectangle dimensions:  0.5? latitud x 0.5? longitude   
- RECT4 <- read.csv("data/SurvRects_4.csv")
- RECT4<-droplevels(RECT4) 
+#Rectangle dimensions:  0.5? latitud x 0.5? longitude 
+
+RECT4 <- read.csv("data/SurvRects_4.csv")
+
+RECT4<-droplevels(RECT4) 
+
 summary(RECT4)
 
-##Importing  file of proportion in rectangles area that correspond sea(removing covered area by land from rectangle area)
-## Rectangle area   (cos(RECT$lat*pi/180)*30*1853.2)*30*1853.2 
+# Importing  file of proportion in rectangles area that correspond sea(removing covered area by land from rectangle area)
+# Rectangle area   (cos(RECT$lat*pi/180)*30*1853.2)*30*1853.2 
  
- RECTall <- read.csv("data/rect_searatio_all.csv")
-  summary(RECTall)
+RECTall <- read.csv("data/rect_searatio_all.csv")
 
-##Matching covered area by land to western area rectangles. Mackerel Western component 
+summary(RECTall)
 
- RECT_west<-join(RECT4[ ,c("lat","lon","r1","R2","RECT","Area")],RECTall[ ,c("lat","lon","sea_ratio" ,"Area_minus_land" )],by=c("lat","lon")) 
- RECT_west$sea_ratio[is.na(RECT_west$sea_ratio)]<-1   #Problem with NA to be gridded. just in case
- RECT_west$Area_minus_land<-RECT_west$Area*RECT_west$sea_ratio
- RECT_west<-droplevels(RECT_west)
+
+# Matching covered area by land to western area rectangles. Mackerel Western component 
+
+RECT_west<-join(RECT4[ ,c("lat","lon","r1","R2","RECT","Area")],RECTall[ ,c("lat","lon","sea_ratio" ,"Area_minus_land" )],by=c("lat","lon"))
  
- RECT<-RECT_west
+RECT_west$sea_ratio[is.na(RECT_west$sea_ratio)]<-1   #Problem with NA to be gridded. just in case
  
-##Transform to Spatial pixel
+RECT_west$Area_minus_land<-RECT_west$Area*RECT_west$sea_ratio
  
- gridded(RECT_west) = ~lon+lat
+RECT_west<-droplevels(RECT_west)
  
-##transform to Spatial Polygon
+RECT_west_df<-RECT_west
  
- RECTwest_p <- as(RECT_west, "SpatialPolygons")
+
+# Covert to Spatial pixel
  
-##PLOTING 
- plot(RECTwest_p)
- slotNames(RECTwest_p)# slot names
-  
-## Original Rectangle names. 
+gridded(RECT_west) = ~lon+lat
+ 
+
+# Convert to Spatial Polygon
+ 
+RECTwest_p <- as(RECT_west, "SpatialPolygons")
+ 
+
+# PLOTING 
+
+plot(RECTwest_p)
+
+slotNames(RECTwest_p)# slot names
+ 
+ 
+# Original Rectangle names
+
 row.names(RECTwest_p) 
 
  
-## Use the spChFIDs() method in maptools to change the  IDs of at least one of your objects to disambiguate them 
- RECTwest_p <- spChFIDs(RECTwest_p, as.character(RECT_west@data[, 3]))
- row.names(RECTwest_p) 
- 
-## join spatialpoligonos ( step by step)  
- rownames(RECT)<-RECT$RECT
- RECT_p<-(RECTwest_p)
+# Use the spChFIDs() method in maptools to change the  IDs of at least one of your objects to disambiguate them 
 
+RECTwest_p <- spChFIDs(RECTwest_p, as.character(RECT_west@data[, 3]))
+
+row.names(RECTwest_p) 
+ 
+
+## join spatialpoligonos ( step by step)
+
+ rownames(RECT_west_df)<-RECT_west_df$RECT
+
+ 
 ## Projection
-proj4string(RECT_p) <- CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84")
-RECT_p<-SpatialPolygonsDataFrame(RECT_p, data=RECT)
  
-save.image("AEPM_grid_mack_Western.RData") 
+proj4string(RECTwest_p) <- CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84")
 
-### import land and ploting
+RECTwest_p<-SpatialPolygonsDataFrame(RECTwest_p, data=RECT_west_df)
+
+
+# Import land and ploting
+
+
+
+png("images/Western_grid2.png",
+    width = 5, height = 7, units = "in", pointsize = 12,
+    bg = "white", res = 600,
+    type = "cairo-png")
 par(mfrow=c(1,1))
-europe<-map(database = "worldHires",  xlim = c(-27,6), ylim = c(41.5,68.5),fill=T,plot=F)
-europe$names
-IDs <- sapply(strsplit(europe$names, ":"), function(x) x[1])
-europemap <- map2SpatialPolygons(europe,IDs=IDs, proj4string=CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84"))
-proj4string(RECT_p) <- CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84")
-plot(RECT_p, border="grey")
-plot(europemap,add=T, fill=T,col="yellow", border="grey")
+europemap<-map(database = "worldHires",  xlim = c(-27,6), ylim = c(41.5,68.5),fill=T,plot=F)
+#europe$names
 
+IDs <- sapply(strsplit(europemap$names, ":"), function(x) x[1])
+
+europemap <- map2SpatialPolygons(europemap,IDs=IDs, proj4string=CRS("+proj=longlat   +ellps=WGS84 +datum=WGS84"))
+
+plot(RECTwest_p, border="grey")
+
+plot(europemap,add=T, fill=T,col="darkgreen", border=grey(0.4))
+
+dev.off()
+
+
+rm(RECT4, RECT_west, RECTall, IDs)
+
+
+save.image("AEPM_grid_mack_Western.RData") 
 
 
